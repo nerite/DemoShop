d9ef12f50f70501308f26cc51f12dc8d8e6a9e74
Neringa Slezeviciene <faulttester@gmail.com>
2022-10-05 17:31:01 +0300

    Added CucumberOptions to UniversalRunner

--- src/test/java/testRunners/UniversalRunner.java
+++ src/test/java/testRunners/UniversalRunner.java
@@ -1,2 +1,16 @@
-package testRunners;public class UniversalRunner {
+package testRunners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        tags = {"@test"},
+        plugin = {"html:target/cucumber", "json:target/cucumber.json"},
+        features = {"src/test/resources/features/"},
+        glue = {"stepDefinitions"}
+)
+
+public class UniversalRunner {
 }
