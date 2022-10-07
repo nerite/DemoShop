96cec4dfc58615c0243f25293abcfb4ac6154f95
Neringa Slezeviciene <faulttester@gmail.com>
2022-10-07 17:47:41 +0300

    Added new mothods

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -3,11 +3,19 @@ package stepDefinitions;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import pageObjects.LoginPage;
 import pageObjects.MainPage;
+import pageObjects.MyAccountPage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginSteps extends GeneralSteps{
 
     private MainPage mainPage = new MainPage(driver);
+    private LoginPage loginPage = new LoginPage(driver);
+    private MyAccountPage myAccPage = new MyAccountPage(driver);
 
     @Before
     public void setUp(){
@@ -27,7 +35,23 @@ public class LoginSteps extends GeneralSteps{
 
     @And("^Enter E-mail Address \"([^\"]*)\"$")
     public void enterEmailAddress(String value){
+        loginPage.enterEmail(value);
+    }
 
+    @And("^Enter Password \"([^\"]*)\"$")
+    public void enterPassword(String value){
+        loginPage.enterPassword(value);
+    }
+
+    @And("^Click Login button$")
+    public void clickLoginButton(){
+        loginPage.clickLoginBtn();
+    }
+
+    @Then("^Verifying that I am on \"([^\"]*)\" page$")
+    public void verifyIAmOnMyAccountPage(String value){
+        assertEquals(value, myAccPage.getMyAccountText());
+        assertTrue(myAccPage.myAccTxt.isDisplayed());
     }
 
 //    @After
