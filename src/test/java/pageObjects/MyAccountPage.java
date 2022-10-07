ac52eb7c9047b055a24386f812be1c7a6366e440
Neringa Slezeviciene <faulttester@gmail.com>
2022-10-07 17:49:51 +0300

    Added locator and new mothods

--- src/test/java/pageObjects/MyAccountPage.java
+++ src/test/java/pageObjects/MyAccountPage.java
@@ -1,2 +1,22 @@
-package pageObjects;public class MyAccountPage {
+package pageObjects;
+
+import flowWorkers.WebDriverLib;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class MyAccountPage extends BasePage{
+
+    @FindBy(how = How.XPATH, using = "//div[@id='content']//h2[text()='My Account']")
+    public WebElement myAccTxt;
+
+    public MyAccountPage(WebDriverLib driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getMyAccountText(){
+        return myAccTxt.getText();
+    }
 }
